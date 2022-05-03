f900e61c8e31180e3d70d9ca3715bde1366244cd
chang <2622310479@qq.com>
2022-05-03 21:49:29 +0800

    feat: 样式结构生成

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -13,7 +13,7 @@ import { isDark, toggleDark } from '~/composables'
       class="icon-btn"
       i-carbon-logo-github
       rel="noreferrer"
-      href="https://github.com/antfu/vitesse-lite"
+      href="https://github.com/Ares-Chang/minesweeper"
       target="_blank"
       title="GitHub"
     />
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,9 +1,53 @@
 <script setup lang="ts">
 
+interface BlockState {
+  x: number // 坐标 X
+  y: number // 坐标 Y
+  revealed?: boolean // 是否翻开
+  mine?: boolean // 是否炸弹
+  flagged?: boolean // 是否被标记
+  adjacentMines?: number // 附近炸弹数
+}
+
+const WIDTH = 10
+const HEIGHT = 10
+const state = reactive(
+  Array.from({ length: HEIGHT }, (_, x) =>
+    Array.from({ length: WIDTH },
+      (_, y): BlockState => ({ x, y })
+    )
+  )
+)
+
+/**
+ * 计算生成炸弹
+ */
+function generateMines() {
+  for (const row of state) {
+    for (const block of row) {
+      block.mine = Math.random() < 0.1
+    }
+  }
+}
+generateMines()
+
+/**
+ * 点击翻牌
+ * @param {Number} row 点击行下标
+ * @param {Number} col 点击列下标
+ */
+function onClick(row: number, col: number) {
+  console.log(row, col);
+}
 </script>
 
 <template>
   <div>
-
+    Minesweeper
+    <div v-for="(row, x) in state">
+      <button v-for="(item, y) in row" w-10 h-10 border hover:bg-gray @click="onClick(x, y)">
+        {{ item.mine ? 'X' : '-' }}
+      </button>
+    </div>
   </div>
 </template>
